1a49a59b6ca8d42431dab32508aba7c5553d486e
Aporoid <Aporoid@hotmail.com>
2019-03-09 18:09:45 -0600

    Character controlling and pitfall

--- Awareness/Assets/Scripts/PlayerController.cs
+++ Awareness/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private float accelerationForce = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private float maxSpeed = 5;
+
+	[SerializeField]
+	private float jumpForce = 3;
+
+	[SerializeField]
+	private Rigidbody2D rb2d;
+
+	[SerializeField]
+	private Collider2D playerGroundCollider;
+
+	[SerializeField]
+	private Collider2D groundDetectTrigger;
+
+	[SerializeField]
+	private ContactFilter2D groundContactFilter;
+
+	[SerializeField]
+	private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+	private float horizontalInput;
+	private bool isOnGround;
+
+	private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+
+	private void Update()
+	{
+		UpdateIsOnGround();
+		UpdateHorizontalInput();
+		HandleJumpInput();
+	}
+
+	private void FixedUpdate()
+	{
+		UpdatePhysicsMaterials();
+		Move();
+	}
+
+	private void Move()
+	{
+		rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
+		Vector2 clampedVelocity = rb2d.velocity;
+		clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+		rb2d.velocity = clampedVelocity;
+	}
+
+	private void UpdateHorizontalInput()
+	{
+		horizontalInput = Input.GetAxisRaw("Horizontal");
+	}
+
+	private void UpdatePhysicsMaterials()
+	{
+		if (Mathf.Abs(horizontalInput) > 0)
+		{
+			playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+		}
+		else
+		{
+			playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+		}
+	}
+
+	private void HandleJumpInput()
+	{
+		if (Input.GetButtonDown("Jump") && isOnGround)
+		{
+			rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+		}
+	}
+
+	private void UpdateIsOnGround()
+	{
+		isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+	}
 }
