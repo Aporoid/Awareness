8a2856a70dee7c65252b993e0cb22984cc710638
Aporoid <Aporoid@hotmail.com>
2019-03-15 13:23:14 -0500

    Finished

---
+++ Awareness/Assets/Scripts/Finish.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+	[SerializeField]
+	private SpriteRenderer spriteRenderer;
+
+	private void Start()
+	{
+		spriteRenderer.enabled = false;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			spriteRenderer.enabled = true;
+		}
+	}
+}
