2b5ae1d234a22129d37b932d383312718ff855da
Aporoid <Aporoid@hotmail.com>
2019-03-18 16:45:50 -0500

    Final touches

--- Awareness/Assets/Scripts/Hazards.cs
+++ Awareness/Assets/Scripts/Hazards.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Hazards : MonoBehaviour
 {
+	private int LevelChange = 0;
+
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.gameObject.CompareTag("Player"))
+		if (collision.gameObject.CompareTag("Player") && LevelChange == 0)
 		{
+			LevelChange++;
 			SceneManager.LoadScene(0);
-		}		
+		}
+		else if (collision.gameObject.CompareTag("Player") && LevelChange == 1)
+		{
+			LevelChange++;
+			SceneManager.LoadScene(1);
+		}
 	}
 }
